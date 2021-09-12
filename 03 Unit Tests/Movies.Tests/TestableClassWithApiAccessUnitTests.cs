8eb97bc59ff1d631dc7a03aa49b0f9a09c0fefea
Andrii Symonenko <andriisymonenko@gmail.com>
2021-09-12 16:20:10 +0300

    chore: added unit tests

---
+++ 03 Unit Tests/Movies.Tests/TestableClassWithApiAccessUnitTests.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+using Movies.Client;
+using Xunit;
+
+namespace Movies.Tests
+{
+    public class TestableClassWithApiAccessUnitTests
+    {
+        [Fact]
+        public async void GetMovie_On401Response_MustThrowUnauthorizedApiAccessException()
+        {
+            var httpClient = new HttpClient(new Return401UnauthorizedResponseHandler())
+            {
+                BaseAddress = new Uri("http://localhost")
+            };
+
+            var testableClass = new TestableClassWithApiAccess(httpClient);
+
+            await Assert.ThrowsAsync<UnauthorizedApiAccessException>(() => testableClass.GetMovie(CancellationToken.None));
+        }
+
+        [Fact]
+        public async void GetMovie_On401Response_MustThrowUnauthorizedApiAccessException_WithMoq()
+        {
+            var unauthorizedResponseHttpMessageHandlerMock = new Mock<HttpMessageHandler>();
+            unauthorizedResponseHttpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.Unauthorized
+                });
+
+            var httpClient = new HttpClient(unauthorizedResponseHttpMessageHandlerMock.Object)
+            {
+                BaseAddress = new Uri("http://localhost")
+            };
+
+            var testableClass = new TestableClassWithApiAccess(httpClient);
+
+            await Assert.ThrowsAsync<UnauthorizedApiAccessException>(() => testableClass.GetMovie(CancellationToken.None));
+        }
+    }
+}
---
+++ Movies.Client/Return401UnauthorizedResponseHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Movies.Client
+{
+    public class Return401UnauthorizedResponseHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            return Task.FromResult(response);
+        }
+    }
+}
